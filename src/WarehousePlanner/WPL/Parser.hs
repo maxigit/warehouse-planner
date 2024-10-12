229f2116466c88502b621ef7932a4dffd15ba99f
Maxime Bourget <bmx007@gmail.com>
2024-10-12 20:27:07 +0100

    Feat(WPL): Changes indenting rules
    Now whole line are parsed
    
       A B C D
           E
    
    is the same as
    
       A B C D
         E
    
    before it was
    
       A B
           C D
           E

--- src/WarehousePlanner/WPL/Parser.hs
+++ src/WarehousePlanner/WPL/Parser.hs
@@ -9,13 +9,25 @@ import WarehousePlanner.Selector
 import WarehousePlanner.Type
 import WarehousePlanner.Base
 -- import WarehousePlanner.Type
-import Text.Megaparsec as P
+import Text.Megaparsec as P hiding((<?>))
+import Text.Megaparsec.Debug qualified as P
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer qualified as L
 import Data.Char
 import Data.List.NonEmpty (NonEmpty(..))
 import Control.Monad(fail)
+import Data.Foldable qualified as F
 
+-- dbg _ = id
+dbg :: Show a => String -> MParser a -> MParser a
+-- dbg = P.dbg
+dbg _ = id
+
+-- avoid redundant import
+_dbg :: Show a => String -> MParser a -> MParser a
+_dbg = P.dbg
+(<?>):: Show a => MParser a -> String -> MParser a
+p <?> lbl = dbg lbl $ label lbl p
 
 -- * Whitespaces & Co
 lineComment :: MParser ()
@@ -26,79 +38,105 @@ blockComment :: MParser ()
 blockComment = L.skipBlockComment "{-" "-}"
 
 -- | Spaces with new lines
-whites :: MParser ()
-whites = L.space space1
+spaces :: MParser ()
+spaces = L.space space1
                  lineComment
-                 blockComment 
+                 blockComment
 
 
 -- | Spaces without new lines
-spaces :: MParser ()
-spaces = L.space (void $ some (char ' ' <|> char '\t'))
+hspaces :: MParser ()
+hspaces = L.space hspace1
                  lineComment
                  empty
 
 lexeme :: MParser a -> MParser a
-lexeme = L.lexeme spaces
+lexeme = L.lexeme hspaces
+lexeme1 p = do
+   r <- p
+   lookAhead $ asum [space1, lineComment, blockComment, eof ]
+   hspaces
+   return r
 
+newLine = hspaces >> (void eol <|> eof)
 wplParser ::  MParser [Statement]
-wplParser = (some $ L.nonIndented whites $ statement True) <*  whites <* eof
- 
+wplParser = (some $ L.nonIndented spaces $ statement) <*  spaces <* eof
 
-{-
+
+statement :: MParser Statement
+statement = asum
+   [ caseBlock <?> "statement:case block"
+   , thenMulti <?> "statement:then multi"
+   ] 
+
+
 caseBlock :: MParser Statement
 caseBlock = do
-   L.indentBlock whites p
-   where p = do
-           a <- lexeme atom
-           e <-  caseLine
-           return $ L.IndentSome Nothing  (return . Then a . Cases . (e:|)) caseLine
-         caseLine = do
-                  lexeme "|"
-                  c <- atom
-                  st <- statement
-                  return $ Case c st
-                  -}
-     
-statement :: Bool -> MParser Statement
-statement first = do
-   iLvl <- L.indentLevel
-   a <- lexeme $ atom first
-   let iguard =  do
-                    eof <|> (void $ L.indentGuard whites GT iLvl)
-   nextm <- optional $ try do
-         iguard
-         statementOrCase
-   whites
-   case nextm of
-      Nothing -> return a
-      Just (Left c) -> do
-           cs <- many $ iguard >> caseLine
-           return $ Then a $ Cases (c :| cs)
-      Just (Right s) -> do
-           ss <- many $ iguard >> statement False
-           return $ Then a $ case ss of 
-                              [] -> s
-                              _ -> Ors (s :| ss )
+  blockOf caseLine Cases
+
+-- | Statements with same indentation
+blockOf :: MParser a -> (NonEmpty a -> b) -> MParser b
+blockOf p mk = do
+  -- consume possible space to be sure to start at the beginning of
+  -- block to get the indentation correct
+  iLvl <- L.indentLevel
+  let iguard = try $ L.indentGuard hspaces EQ iLvl <?> ("blockGuard " <> show iLvl)
+  cs <- some $ iguard >> p
+  case cs of
+     [] -> fail "some returning []"
+     c:cs -> return $ mk (c :| cs)
+
+thenLine :: MParser Statement
+thenLine =do
+  a <- atom
+  thenm <- optional $ try $ asum [  caseBlock <?> "case in line"
+                                 , thenLine <?> "next in line"
+                                 ]
+  case thenm of
+     Nothing -> return a <* (newLine <?> "end of thenLine")
+     Just then_ -> return $ a `Then` then_
+
+thenMulti :: MParser Statement
+thenMulti = do
+  iLvl <- L.indentLevel
+  line <- thenLine <?> "thenMulti:line"
+  spaces
+  -- get different blocks of decreasing indentation
+  childrenm <- many $ (L.indentGuard spaces GT iLvl <?> ("thenMulti:guard " <> show iLvl))
+                        >> (orBlock <?> "thenMulti:children")
+  return case childrenm of
+         [] -> line
+         ors -> F.foldl1 Then (line :| ors)
+         --     ^^^^^^^^
+         --       [a, b, c] ->  (a Then b) Then c
+
+orBlock = do
+  blockOf ((caseBlock <?> "caseBlock")
+          <|>
+          (thenLine <?> "line block")
+          ) mkOrs
+
+mkOrs :: NonEmpty Statement -> Statement
+mkOrs ors = case ors of
+   o :| [] -> o
+   _ -> Ors ors
 
 
-statementOrCase :: MParser (Either Case Statement)
-statementOrCase = fmap Left caseLine <|> fmap Right (statement False)
 
 caseLine :: MParser Case
 caseLine = do
-    lexeme "|"
-    c <- statement False
-    return case c of 
+    lexeme "|" <?> "start caseline"
+    c <- thenLine
+    return case c of
        Then a b -> Case a (Just b)
        _ -> Case c Nothing
-          
-atom :: Bool -> MParser Statement
-atom first = (PassThrought <$> (lexeme ";" *> statement first))
-       <|> (lexeme "("  *> statement first <* lexeme ")")
-       <|> (notFollowedBy "|" >> Action <$> command first)
 
-command first = asum $ map lexeme [ toggleTag
+atom :: MParser Statement
+atom = (PassThrought <$> (lexeme ";" *> statement ))
+       <|> ("("  *> statement <* ")")
+       <|> (notFollowedBy "|" >> Action <$> command )
+
+command = asum $ map lexeme [ toggleTag
                             , tag
                             , move
                             , shelfSel
@@ -109,72 +147,72 @@ command first = asum $ map lexeme [ toggleTag
                             , partitionMode
                             , orientationStrategies
                             ] where
-   move = do 
-            lexeme "to" 
-            pmode <- optional $ lexeme partitionModeParser
-            orules <- (lexeme "orules"  >> orientationRules) <|> return []
+   move = do
+            lexeme1 "to"
+            pmode <- optional $ lexeme1 partitionModeParser
+            orules <- (lexeme1 "orules"  >> orientationRules) <|> return []
             shelf <-  shelfSelector
             return $ Move Nothing pmode orules shelf
-   tag = do 
-           lexeme "tag"
-           tagOps <- lexeme $ takeWhile1P (Just "tags") (not . isSpace)
-           return $ Tag (parseTagOperations tagOps) 
-   toggleTag = do 
-           lexeme "tog"
-           tagOps <- lexeme $ takeWhile1P (Just "tags") (not . isSpace)
-           return $ ToggleTags (parseTagOperations tagOps) 
+   tag = do
+           lexeme1 "tag"
+           tagOps <- lexeme1 $ takeWhile1P (Just "tags") (not . isSpace)
+           return $ Tag (parseTagOperations tagOps)
+   toggleTag = do
+           lexeme1 "tog"
+           tagOps <- lexeme1 $ takeWhile1P (Just "tags") (not . isSpace)
+           return $ ToggleTags (parseTagOperations tagOps)
    tam = do
-          lexeme "tam"
-          tagloc <- lexeme $ takeWhile1P (Just "loc#tag") (not . isSpace)
-          ors <- (lexeme "with" >> orientationRules)
+          lexeme1 "tam"
+          tagloc <- lexeme1 $ takeWhile1P (Just "loc#tag") (not . isSpace)
+          ors <- (lexeme1 "with" >> orientationRules)
                  <|> return []
           return $ TagAndMove tagloc ors
-   delete = lexeme "delete" >> return Delete
+   delete = lexeme1 "delete" >> return Delete
    traceCount = do
-       lexeme "trace:count"
-       desc <- lexeme $ takeWhile1P (Just "description") (not . isSpace)
+       lexeme1 "trace:count"
+       desc <- lexeme1 $ takeWhile1P (Just "description") (not . isSpace)
        return $ TraceCount desc
    partitionMode = do
-       lexeme  "place"
-       pmode <- lexeme partitionModeParser
+       lexeme1  "place"
+       pmode <- lexeme1 partitionModeParser
        return $ SetPartitionMode pmode
    orientationStrategies = do
-      lexeme "orules"
-      os <- lexeme orientationRules
+      lexeme1 "orules"
+      os <- lexeme1 orientationRules
       return $ SetOrientationStrategies os
 
-                    
-   boxSel = SelectBoxes <$> boxSelector first
-   shelfSel = SelectShelves <$> asum [ lexeme "/"  >> shelfSelector 
-                                     , lexeme "with" >> cselector parseShelfSelector
+
+   boxSel = SelectBoxes <$> boxSelector 
+   shelfSel = SelectShelves <$> asum [ lexeme "/"  >> shelfSelector
+                                     , lexeme1 "with" >> cselector parseShelfSelector
                                      ]
 
 
-boxSelector :: Bool -> MParser (CSelector BoxSelector)
-boxSelector first =  label "box selector" $ asum
-    [  lexeme "in" >> cselector ((\sel -> selectAllBoxes { shelfSelectors = sel} ) . parseSelector)
-    , guardLower >> cselector (parseBoxSelectorWithDef $ first == True)
+boxSelector :: MParser (CSelector BoxSelector)
+boxSelector =  label "box selector" $ asum
+    [  lexeme1 "in" >> cselector ((\sel -> selectAllBoxes { shelfSelectors = sel} ) . parseSelector)
+    , guardLower >> cselector (parseBoxSelectorWithDef False)
     ]
-    
+
 shelfSelector :: MParser (CSelector ShelfSelector)
-shelfSelector = label "shelf selector" $ asum 
-     [ lexeme "with" >> cselector parseShelfSelector
+shelfSelector = label "shelf selector" $ asum
+     [ lexeme1 "with" >> cselector parseShelfSelector
      , cselector (ShelfSelector SelectAnything . parseSelector)
      ]
 isSelector :: Char -> Bool
 isSelector c = not $ isSpace c || c == ')'
-   
-   
+
+
 cselector :: (Text -> s) -> MParser (CSelector s)
 cselector mk = try $ asum [swapContext, root, parent, stmt, sel ] where
      swapContext = do
          (string "-~")
          return SwapContext
-     sel = CSelector . mk <$> lexeme (takeWhile1P (Just "selector") isSelector)
+     sel = CSelector . mk <$> lexeme1 (takeWhile1P (Just "selector") isSelector)
      parent = (lexeme $ char '~') >> return Parent
      root = (lexeme $ string ".~") >> return Root
-     stmt = CStatement <$> (lexeme "(" *> statement False <* ")")
-         
+     stmt = CStatement <$> (lexeme "(" *> statement <* lexeme ")")
+
 orientationRules :: MParser [OrientationStrategy]
 orientationRules = do
   rule <- lexeme $ takeWhile1P (Just "orientation rules") (not . isSpace)
@@ -187,42 +225,3 @@ orientationRules = do
 -- or escape with `
 guardLower :: MParser ()
 guardLower = label "Escape lower case with `" $ void (char '`') <|> notFollowedBy lowerChar
-{-
-  spaces
-  parseStatement <* eof
-
-parseStatement :: MParser Statement
-parseStatement =  do
-    statement <- unit
-    (opm, nextm) <- do
-              op <- optional $ lexeme ( "~" <|> ";" <|> "|")
-              nextm' <- optional parseStatement
-              return (op, nextm')
-
-    case nextm of
-       Nothing -> return statement
-       Just next -> return $ case opm of 
-                              Just "~" -> Else statement next
-                              Just ";" -> Union statement next
-                              Just "|" -> Skip statement next
-                              _ -> Then statement next
-    where unit = lexeme $ asum [ "(" *> parseStatement <* ")"
-                               , parseAction
-                               ]
-    
--- parseElse :: Statement -> MParser Statement
--- parseElse = "~" *> flip Else <$> parseStatement
-
-parseCommand :: MParser Command
-parseCommand = asum $ map lexeme [ move, shelfSelector, boxSelector] where
-   move = do 
-            "move" 
-            shelf <- takeWhile1P (Just "shelf selector") isSelector
-            return $ Move Nothing $ Just $ parseShelfSelector shelf
-   boxSelector = SelectBoxes . parseBoxSelector <$> takeWhile1P (Just "box selector") isSelector
-   shelfSelector = "/" >> SelectShelves . ShelfSelector SelectAnything . parseSelector <$> takeWhile1P (Just "shelf selector") isSelector
-   isSelector c = not $ isSpace c || c == ')'
-   
-parseAction :: MParser Statement
-parseAction = Action <$> parseCommand
--}
