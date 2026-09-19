632ecb8d245e09d3a7ac500d548b93cb3a8e1755
Maxime Bourget <bmx007@gmail.com>
2026-09-19 12:23:30 +0100

    Fix(extractDimensions): set dim to 0 if 'l are not present

--- src/WarehousePlanner/Base.hs
+++ src/WarehousePlanner/Base.hs
@@ -726,6 +726,7 @@ extractDimensions dim tags = case (go "'l", go "'w", go "'h") of
                                 }
   where go prefix = case traverse readMay $ maybe [] (Set.toList) (Map.lookup prefix tags) of
           Nothing -> Nothing
+          Just [] -> Nothing
           Just values -> Just $ sum values /100
 
 -- | Change the dimension of the box according to its tag
